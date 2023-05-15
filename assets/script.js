e476838bdb53e806be6cc25182faad2696b2f62f
Dayne Lalmond <dlalmondo@gmail.com>
2023-05-15 01:08:50 -0400

    intersection observer

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+const wrapper = document.querySelector('.sections-effect');
+const sections = document.querySelectorAll('.section');
+const className = 'in-view';
+
+wrapper.classList.remove('no-js');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add(className);
+      return;
+    }
+
+    entry.target.classList.remove(className);
+  });
+}, {
+  rootMargin: '0px 0px -250px 100px'
+});
+
+sections.forEach(section => observer.observe(section));
+
+
+function myFunction() {
+    var copyText = document.getElementById("address").title;
+    document.addEventListener('copy', function(event) {
+      event.clipboardData.setData('text/plain', copyText);
+      event.preventDefault();
+      document.removeEventListener('copy', handler, true);
+    }, true);
+    document.execCommand('copy');
+    document.getElementById('notice').innerHTML = "Copied to Clipboard";
+  }
+
+  document.getElementById('address').addEventListener('click', myFunction);
