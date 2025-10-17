a4e9b9810e6f020f92dbcf25bfff0c0df5d50bcd
DayneLalmond <daynelalmond@protonmail.com>
2025-10-17 13:17:47 -0400

    media query nav

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
 
 function myFunction() {
     var copyText = document.getElementById("address").title;
-    document.addEventListener('copy', function(event) {
-      event.clipboardData.setData('text/plain', copyText);
-      event.preventDefault();
-      document.removeEventListener('copy', handler, true);
-    }, true);
-    document.execCommand('copy');
-    document.getElementById('notice').innerHTML = "Copied to Clipboard";
+    // document.addEventListener('copy', function(event) {
+    //   event.clipboardData.setData('text/plain', copyText);
+    //   event.preventDefault();
+    //   document.removeEventListener('copy', handler, true);
+    // }, true);
+    // document.execCommand('copy');
+    document.getElementById('notice').innerHTML = "Loading...";
   }
 
   document.getElementById('address').addEventListener('click', myFunction);
